4f25f1485b54bc0e2376445bdc11f1422cab2eb2
Your Name <jasonbataillon@gmail.com>
2024-10-02 02:14:54 -0400

    Was not easy, but got it done.

--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,10 +1,44 @@
 import React, { useState, useEffect } from 'react';
 
-function App({ selectedContactId, setSelectedContactId }) {
+export default function SelectedContact({
+  selectedContactId,
+  setSelectedContactId,
+}) {
+  const [contact, setContact] = useState(null);
+
+  useEffect(() => {
+    if (selectedContactId) {
+      async function fetchContact() {
+        try {
+          const response = await fetch(
+            `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${selectedContactId}`
+          );
+          const result = await response.json();
+          setContact(result);
+          console.log(result);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      fetchContact();
+    }
+  }, [selectedContactId]);
+
   return (
     <div>
-      <h1>Selected Contact ID: {selectedContactId}</h1>
-      {sadasda}
+      {contact ? (
+        <div>
+          <h2>Selected Contact Details</h2>
+          <p>Name: {contact.name}</p>
+          <p>Email: {contact.email}</p>
+          <p>Phone: {contact.phone}</p>
+          <button onClick={() => setSelectedContactId(null)}>
+            Back to Contact List
+          </button>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
